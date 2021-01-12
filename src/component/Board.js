40a1c4576c9bb1dad9b6319a74dc07781da979a9
weerapath ruenrurngdee <m17661weerapast@gmail.com>
2021-01-12 09:54:05 +0700

    can play online!

--- src/component/Board.js
+++ src/component/Board.js
@@ -19,7 +19,6 @@ import firebase from '../firebase'
 function Board(props){
 
     //player data
-    const [playerrole,Setplayerrole] = useState(props.playerrole)
     const [FrontRow,SetFrontRow] = useState(['empty_card.jpg','empty_card.jpg','empty_card.jpg'])
     const [BackRow,SetBackRow] = useState(['empty_card.jpg','empty_card.jpg'])
     const [Hand,SetHand] = useState([])
@@ -40,139 +39,60 @@ function Board(props){
     //update real-time player board
     const updateplayerdata = ()=>{
         const db = firebase.firestore()
-        let data = {}
-        //console.log('update')
-        if(playerrole === 'player1'){
-            console.log('update player1')
-            data = {
-                player1deck:Deck,
-                player1backrow:BackRow,
-                player1checkzone:CheckZone,
-                player1climaxzone:ClimaxZone,
-                player1clock:Clock,
-                player1frontrow:FrontRow,
-                player1hand:Hand,
-                player1level:Level,
-                player1memory:Memory,
-                player1stock:Stock,
-                player1waitingroom:WaitingRoom,
-            }
-        }else if(playerrole === 'player2'){
-            console.log('update player2')
-            data = {
-                player2deck:Deck,
-                player2backrow:BackRow,
-                player2checkzone:CheckZone,
-                player2climaxzone:ClimaxZone,
-                player2clock:Clock,
-                player2frontrow:FrontRow,
-                player2hand:Hand,
-                player2level:Level,
-                player2memory:Memory,
-                player2stock:Stock,
-                player2waitingroom:WaitingRoom,
-            }
+        let data = {
+            deck:Deck,
+            backrow:BackRow,
+            checkzone:CheckZone,
+            climaxzone:ClimaxZone,
+            clock:Clock,
+            frontrow:FrontRow,
+            hand:Hand,
+            level:Level,
+            memory:Memory,
+            stock:Stock,
+            waitingroom:WaitingRoom,
         }
-        db.collection("board").doc("1234").update(data)
+        db.collection("board").doc(props.playerName === 'butter' ? "1234" : "5678").update(data)
         .then(()=>{})
         .catch(err=>console.log(err))
     }
 
     useEffect(updateplayerdata,[Deck,BackRow,CheckZone,ClimaxZone,Clock,
-        FrontRow,Hand,Level,Memory,Stock,WaitingRoom,playerrole])
+        FrontRow,Hand,Level,Memory,Stock,WaitingRoom,props.playerName])
     
     //opponent data
-    const [OpponentFrontRow,SetOpponentFrontRow] = useState(['empty_card.jpg','empty_card.jpg','empty_card.jpg'])
-    const [OpponentBackRow,SetOpponentBackRow] = useState(['empty_card.jpg','empty_card.jpg'])
+    const [OpponentFrontRow,SetOpponentFrontRow] = useState([])
+    const [OpponentBackRow,SetOpponentBackRow] = useState([])
     const [OpponentHand,SetOpponentHand] = useState([])
-    const [OpponentClock,SetOpponentClock] = useState(['empty_card.jpg',
-        'empty_card.jpg',
-        'empty_card.jpg',
-        'empty_card.jpg',
-        'empty_card.jpg',
-        'empty_card.jpg'])
+    const [OpponentClock,SetOpponentClock] = useState([])
     const [OpponentStock,SetOpponentStock] = useState([])
     const [OpponentLevel,SetOpponentLevel] = useState([])
-    const [OpponentDeck,SetOpponentDeck] = useState(props.opponentDeck)
+    const [OpponentDeck,SetOpponentDeck] = useState([])
     const [OpponentWaitingRoom,SetOpponentWaitingRoom] = useState([])
     const [OpponentMemory,SetOpponentMemory] = useState([])
     const [OpponentCheckZone,SetOpponentCheckZone] = useState([])
     const [OpponentClimaxZone,SetOpponentClimaxZone] = useState([])
-    
-    const [Unknown,SetUnKnown] = useState([])
-    //update real-time opponent board
-    // const db = firebase.firestore()
-    // let test = ''
-    
-    // db.collection("board").doc("1234") // test board id is "1234"
-    //     .onSnapshot(function(doc) {
-    //         console.log(doc.data().player1backrow)
-    //         test = doc.data().player1backrow
-    //         SetUnKnown('hello')
-        //    SetUnKnown(doc.data().player1backrow)
-        //     if(playerrole === 'player1'){
-        //         SetOpponentBackRow(doc.data().player2backrow)
-        //         SetOpponentFrontRow(doc.data().player2frontrow)
-        //         SetOpponentHand(doc.data().player2hand)
-        //         SetOpponentClock(doc.data().player2clock)
-        //         SetOpponentStock(doc.data().player2stock)
-        //         SetOpponentLevel(doc.data().player2level)
-        //         SetOpponentDeck(doc.data().player2deck)
-        //         SetOpponentWaitingRoom(doc.data().player2waitingroom)
-        //         SetOpponentMemory(doc.data().player2memory)
-        //         SetOpponentCheckZone(doc.data().player2checkzone)
-        //         SetOpponentClimaxZone(doc.data().player2climaxzone)   
-        //     }else if(playerrole === 'player2'){
-        //         SetOpponentBackRow(doc.data().player1backrow)
-        //         SetOpponentFrontRow(doc.data().player1frontrow)
-        //         SetOpponentHand(doc.data().player1hand)
-        //         SetOpponentClock(doc.data().player1clock)
-        //         SetOpponentStock(doc.data().player1stock)
-        //         SetOpponentLevel(doc.data().player1level)
-        //         SetOpponentDeck(doc.data().player1deck)
-        //         SetOpponentWaitingRoom(doc.data().player1waitingroom)
-        //         SetOpponentMemory(doc.data().player1memory)
-        //         SetOpponentCheckZone(doc.data().player1checkzone)
-        //         SetOpponentClimaxZone(doc.data().player1climaxzone)
-            // }
-        // }, function(error) {
-        //     console.log(error)
-        // })
-    
+        
     useEffect(() => {
         const db = firebase.firestore()
-        db.collection("board").doc("1234")
+        db.collection("board").doc(props.playerName === 'butter' ? "5678" : "1234")
         .onSnapshot(function(doc) {
             console.log(doc.data())
-            if(playerrole === 'player1'){
-                SetOpponentBackRow(doc.data().player2backrow)
-                SetOpponentFrontRow(doc.data().player2frontrow)
-                SetOpponentHand(doc.data().player2hand)
-                SetOpponentClock(doc.data().player2clock)
-                SetOpponentStock(doc.data().player2stock)
-                SetOpponentLevel(doc.data().player2level)
-                SetOpponentDeck(doc.data().player2deck)
-                SetOpponentWaitingRoom(doc.data().player2waitingroom)
-                SetOpponentMemory(doc.data().player2memory)
-                SetOpponentCheckZone(doc.data().player2checkzone)
-                SetOpponentClimaxZone(doc.data().player2climaxzone)   
-            }else if(playerrole === 'player2'){
-                SetOpponentBackRow(doc.data().player1backrow)
-                SetOpponentFrontRow(doc.data().player1frontrow)
-                SetOpponentHand(doc.data().player1hand)
-                SetOpponentClock(doc.data().player1clock)
-                SetOpponentStock(doc.data().player1stock)
-                SetOpponentLevel(doc.data().player1level)
-                SetOpponentDeck(doc.data().player1deck)
-                SetOpponentWaitingRoom(doc.data().player1waitingroom)
-                SetOpponentMemory(doc.data().player1memory)
-                SetOpponentCheckZone(doc.data().player1checkzone)
-                SetOpponentClimaxZone(doc.data().player1climaxzone)
-            }
+            SetOpponentBackRow(doc.data().backrow)
+            SetOpponentFrontRow(doc.data().frontrow)
+            SetOpponentHand(doc.data().hand)
+            SetOpponentClock(doc.data().clock)
+            SetOpponentStock(doc.data().stock)
+            SetOpponentLevel(doc.data().level)
+            SetOpponentDeck(doc.data().deck)
+            SetOpponentWaitingRoom(doc.data().waitingroom)
+            SetOpponentMemory(doc.data().memory)
+            SetOpponentCheckZone(doc.data().checkzone)
+            SetOpponentClimaxZone(doc.data().climaxzone)   
         }, function(error) {
             console.log(error)
         });
-    }, [])
+    }, [props.playerName])
 
     /*Drag-Drop */
     const [src,Setsrc] = useState("empty_card.jpg")
@@ -204,7 +124,7 @@ function Board(props){
             if(n !== -1){
                 let slot_array = [Hand,Stock,Level,WaitingRoom,Deck,Memory,CheckZone,ClimaxZone]
                 let Setslot_array = [SetHand,SetStock,SetLevel,SetWaitingRoom,SetDeck,SetMemory,SetCheckZone,SetClimaxZone]
-                let Dummy = slot_array[n]
+                let Dummy = [...slot_array[n]]
                 Dummy.push(src)
                 Setslot_array[n](Dummy)
             }else{
@@ -212,7 +132,7 @@ function Board(props){
                 let slot_array = [FrontRow,BackRow,Clock]
                 let Setslot_array = [SetFrontRow,SetBackRow,SetClock]
                 n = slot.indexOf(e.target.attributes['zone'].value)
-                let Dummy = slot_array[n]
+                let Dummy = [...slot_array[n]]
                 Setswap(Dummy[parseInt(e.target.attributes[0].value)]) // swap card if card over in frontrow or backrow
                 if(Dummy[parseInt(e.target.attributes[0].value)] !== 'empty_card.jpg' && (zone!=='frontrow' && zone!=='backrow')){ // add overed card in waiting room
                     WaitingRoom.push(Dummy[parseInt(e.target.attributes[0].value)])
@@ -227,40 +147,40 @@ function Board(props){
     const DragEnd = (e) =>{
         if(drop){
             if(e.target.attributes['zone'].value === 'frontrow'){
-                let DummyFrontRow = FrontRow
+                let DummyFrontRow = [...FrontRow]
                 DummyFrontRow[parseInt(e.target.attributes[0].value)] = swap
                 SetFrontRow(DummyFrontRow)
             }else if(e.target.attributes['zone'].value === 'backrow'){
-                let DummyBackRow = BackRow
+                let DummyBackRow = [...BackRow]
                 DummyBackRow[parseInt(e.target.attributes[0].value)] = swap
                 SetBackRow(DummyBackRow)
             }else if(e.target.attributes['zone'].value === 'clock'){
-                let DummyClock = Clock
+                let DummyClock = [...Clock]
                 DummyClock[parseInt(e.target.attributes[0].value)] = 'empty_card.jpg'
                 SetClock(DummyClock)
             }else if(e.target.attributes['zone'].value === 'stock'){
-                let DummyStock = Stock
+                let DummyStock = [...Stock]
                 DummyStock.pop()
                 SetStock(DummyStock)
             }else if(e.target.attributes['zone'].value === 'deck'){
-                let DummyDeck = Deck
+                let DummyDeck = [...Deck]
                 DummyDeck.pop()
                 SetDeck(DummyDeck)
             }else if(e.target.attributes['zone'].value === 'level'){
-                let DummyLevel = Level
+                let DummyLevel = [...Level]
                 DummyLevel.pop()
                 SetLevel(DummyLevel)
             }else if(e.target.attributes['zone'].value === 'checkzone'){
-                let DummyCheckZone = CheckZone
+                let DummyCheckZone = [...CheckZone]
                 DummyCheckZone.splice(parseInt(e.target.attributes['index'].value),1)
                 SetCheckZone(DummyCheckZone)
             }else if(e.target.attributes['zone'].value === 'climaxzone'){
-                let DummyClimaxZone = ClimaxZone
+                let DummyClimaxZone = [...ClimaxZone]
                 DummyClimaxZone.splice(parseInt(e.target.attributes['index'].value),1)
                 SetClimaxZone(DummyClimaxZone)
             }
             else{
-                let DummyHand = Hand
+                let DummyHand = [...Hand]
                 DummyHand.splice(parseInt(e.target.attributes['index'].value),1)
                 SetHand(DummyHand)
             }
