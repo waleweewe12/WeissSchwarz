32d553426ea0f5581c9db623953a16aa01034ba2
weerapath ruenrurngdee <m17661weerapast@gmail.com>
2021-01-13 23:02:21 +0700

    FrontRow.js add rotate function

--- src/component/BackRow.js
+++ src/component/BackRow.js
@@ -3,23 +3,25 @@ import style from '../style/BoardStyle'
 
 function BackRow(props){
     return(
-        <div className="col-4">
-            <div className="row justify-content-center">
-                {props.BackRow.map((item,i)=><img 
-                    key={i} 
-                    index={i} // index of array
-                    zone='backrow' // type of array
-                    style={item === 'empty_card.jpg' ? {...style.CardStyle,...style.EmptycardStyle} : style.CardStyle}  
-                    src={item}
-                    draggable="true"
-                    onDragStart={props.DragStart} 
-                    onDragOver={props.DragOver}
-                    onDrop={props.DragDrop} 
-                    onDragEnd={props.DragEnd}
-                    alt="..."/>)
-                }
-            </div>
-        </div>
+        <>
+            {props.BackRow.map((item,i)=>
+                <div className="col-2">
+                    <img 
+                        key={i} 
+                        index={i} // index of array
+                        zone='backrow' // type of array
+                        style={item === 'empty_card.jpg' ? {...style.CardStyle,...style.EmptycardStyle} : style.CardStyle}  
+                        src={item}
+                        draggable="true"
+                        onDragStart={props.DragStart} 
+                        onDragOver={props.DragOver}
+                        onDrop={props.DragDrop} 
+                        onDragEnd={props.DragEnd}
+                        alt="..."
+                    />
+                </div>
+            )}
+        </>
     )
 }
 
--- src/component/FrontRow.js
+++ src/component/FrontRow.js
@@ -9,6 +9,11 @@ function FrontRow(props){
         dummyTap[e.target.attributes['index'].value] = true
         SetTap(dummyTap)
     }
+    const HandleUntapClicked = (e)=>{
+        let dummyTap = [...Tap]
+        dummyTap[e.target.attributes['index'].value] = false
+        SetTap(dummyTap)
+    }
 
     const TapStyle = {
         transform: 'rotate(90deg)',
@@ -16,87 +21,48 @@ function FrontRow(props){
         marginRight:'5px'
     }
 
+    const ReverseStyle = {
+        transform: 'rotate(360deg)'
+    }
+
     return(
         <div className="row justify-content-center" style={{marginBottom:"10px"}}>
-            {props.FrontRow.map((item,i)=><img 
-                key={i} 
-                index={i} // index of array
-                zone='frontrow' // type of array
-                style={
-                    item === 'empty_card.jpg' ? {...style.CardStyle,...style.EmptycardStyle} :  
-                    Tap[i] ? {...style.CardStyle,...TapStyle} : style.CardStyle
-                } 
-                src={item}  
-                draggable="true"
-                onDragStart={props.DragStart}
-                onDragOver={props.DragOver}
-                onDrop={props.DragDrop} 
-                onDragEnd={props.DragEnd}
-                onClick={HandleTapClicked}
-                alt="..."/>)
-            }
-            <div 
-                style={{
-                    position:'absolute',
-                    width:'80px',
-                    backgroundColor:'white',
-                    padding:'0',
-                    top:'45%',
-                    left:'58%',
-                    padding:'2px',
-                    border:'1px solid black',
-                    borderRadius:'10px'
-                }}
-            >
-                <button style={{width:'100%',borderRadius:'10px'}}>Tap</button>
-                <button style={{width:'100%',borderRadius:'10px'}}>UnTap</button>
-                <button style={{width:'100%',borderRadius:'10px'}}>Reverse</button>
-            </div>
+            {props.FrontRow.map((item,i)=>
+                <div className="col-2">
+                    <img 
+                        key={i} 
+                        index={i} // index of array
+                        zone='frontrow' // type of array
+                        style={
+                            item === 'empty_card.jpg' ? {...style.CardStyle,...style.EmptycardStyle} :  
+                            Tap[i] ? {...style.CardStyle,...TapStyle} : style.CardStyle
+                        } 
+                        src={item}  
+                        draggable="true"
+                        onDragStart={props.DragStart}
+                        onDragOver={props.DragOver}
+                        onDrop={props.DragDrop} 
+                        onDragEnd={props.DragEnd}
+                        onClick={HandleTapClicked}
+                        alt="..."
+                    />
+                    <div style={{position:'absolute',width:'70px',zIndex:'2',display:'inline-block'}}>
+                        <button index={i} style={{width:'100%'}}>Untap</button>
+                        <button index={i} style={{width:'100%'}} onClick={HandleTapClicked}>Tap</button>
+                        <button index={i} style={{width:'100%'}}>Reverse</button>
+                    </div>
+                </div>
+            )}
+                {/* <div
+                    style={{
+                        width:'100px',
+                        height:'100px',
+                        backgroundColor:'white',
+                        top:'20%'
+                    }}
+                >
+                </div> */}
 
-            <div 
-                style={{
-                    position:'absolute',
-                    width:'80px',
-                    backgroundColor:'white',
-                    padding:'0',
-                    left:'63%'
-                }}
-            >
-                <button style={{width:'100%'}}>Tap</button>
-            </div>
-
-            <div 
-                style={{
-                    position:'absolute',
-                    width:'80px',
-                    backgroundColor:'white',
-                    padding:'0',
-                    left:'68%'
-                }}
-            >
-                <button style={{width:'100%'}}>Tap</button>
-            </div>
-            {/* <div 
-                style={{
-                    position:'absolute',
-                    width:'80px',
-                    backgroundColor:'white',
-                    padding:'0',
-                    left:'100px'
-                }}
-            >
-                <button style={{width:'100%'}}>Tap</button>
-            </div>
-            <div 
-                style={{
-                    position:'absolute',
-                    width:'80px',
-                    backgroundColor:'white',
-                    padding:'0'
-                }}
-            >
-                <button style={{width:'100%'}}>Tap</button>
-            </div> */}
         </div>
     )
 }
